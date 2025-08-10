945b7bdbae695bcc6193184eb6ce4209124d277b
Albert Luppol <albert.y.luppol@gmail.com>
2025-08-10 16:08:33 -0700

    auth: personController

--- src/test/java/com/luppol/life_balance/controllers/PersonControllerIT.java
+++ src/test/java/com/luppol/life_balance/controllers/PersonControllerIT.java
@@ -1,28 +1,22 @@
 package com.luppol.life_balance.controllers;
 
 import com.jayway.jsonpath.JsonPath;
-import com.luppol.life_balance.mappers.PersonMapper;
-import com.luppol.life_balance.models.Person;
-import com.luppol.life_balance.repositories.PersonRepository;
-import com.luppol.life_balance.services.PersonService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 @Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
